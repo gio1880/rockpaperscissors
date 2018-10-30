0afa08c7f428a6ba399fed69f3abf742f4f96678
Giovanny De Los Angeles <giovannydelosangeles@gmail.com>
2018-10-30 22:25:31 +0000

    tied situation created

--- js/script.js
+++ js/script.js
@@ -6,6 +6,20 @@
 /* global $ */
 
 
-
 // DOCUMENT READY FUNCTION BELOW
+$("#shoot").click(function(){
+    var input = $("#input").val();
+var choices = ["rock", "paper", "scissors"]
 
+var randomIndex = Math.floor(Math.random() * choices.length); 
+var randomElement = choices[randomIndex];
+
+    $("#userChoice").text(input);
+    $("#computerChoice").text(randomElement);
+    if(input === randomElement){
+        $("#result").text("Tied");        
+    }
+    else if(input==="paper" || randomElement==="rock"){
+        $("#result").text("Computer Won")
+    }
+});
