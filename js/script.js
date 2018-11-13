6718363cf64a81185dc7f50425dae3fe1e79835e
Giovanny De Los Angeles <giovannydelosangeles@gmail.com>
2018-11-13 22:03:46 +0000

    Logic Update

--- js/script.js
+++ js/script.js
@@ -4,22 +4,40 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-
-
-// DOCUMENT READY FUNCTION BELOW
-$("#shoot").click(function(){
-    var input = $("#input").val();
 var choices = ["rock", "paper", "scissors"]
 
 var randomIndex = Math.floor(Math.random() * choices.length); 
 var randomElement = choices[randomIndex];
 
+// DOCUMENT READY FUNCTION BELOW
+$("#shoot").click(function(){
+    var input = $("#input").val();
+    // input values 
     $("#userChoice").text(input);
     $("#computerChoice").text(randomElement);
+    // Tied Situation
     if(input === randomElement){
         $("#result").text("Tied");        
     }
-    else if(input==="paper" || randomElement==="rock"){
+    // paper and rock
+    else if(input==="paper" && randomElement==="rock"){
+        $("#result").text("Player Won")
+    }
+    else if(input==="rock" && randomElement==="paper"){
         $("#result").text("Computer Won")
+    }
+    // scissors and rock
+    else if(input==="scissors" && randomElement==="rock"){
+        $("#result").text("Computer Won")
+    }
+    else if(input==="rock" && randomElement==="scissors"){
+        $("#result").text("Player Won")
+    }
+    // scissors and paper
+    else if(input==="paper" && randomElement==="scissors"){
+        $("#result").text("Computer Won")
+    }
+    else if(input==="scissors" && randomElement==="paper"){
+        $("#result").text("Player Won")
     }
 });
